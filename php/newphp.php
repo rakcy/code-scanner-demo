cfe24730e577627cfcc1d8f5c45bb2d942df8510
Hadrian Bayanulhaq Siregar <hadrianbayanulhaq@gmail.com>
2019-09-16 11:45:01 +0700

    duplicates

---
+++ php/newphp.php
@@ -0,0 +1,34 @@
+<?php
+class Foo {
+    public function bar($param)  {
+        if ($param === 42) {
+            eval('$param = 23;');
+        }
+    }
+}
+echo 'Hello World';
+// echo 'Hello World';
+
+// trigger RSPEC-2068
+$uname = "jeff";
+$password = "hunter2";
+connect($uname, $password);
+
+
+// trigger RSPEC-2068
+$uname = "jeff";
+$password = "hunter2";
+connect($uname, $password);
+
+
+// trigger RSPEC-2068
+$uname = "jeff";
+$password = "hunter2";
+connect($uname, $password);
+
+
+// trigger RSPEC-2068
+$uname = "jeff";
+$password = "hunter2";
+connect($uname, $password);
+?>
